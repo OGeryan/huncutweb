b8df34528df481d243516a2f83a43ae78965398a
OGeryan <88555297+OGeryan@users.noreply.github.com>
2023-04-18 22:53:49 +0200

    test numero dos

--- huncutindex.php
+++ huncutindex.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blender</title>
-    <link href="huncutstyle.css" rel="text/css" type="stylesheet">
+    <link href="huncutstyle.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <header class="HEAD">
