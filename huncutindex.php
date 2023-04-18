dec7b4e32b21007090a6fe6216cdbc0fa43b9194
OGeryan <88555297+OGeryan@users.noreply.github.com>
2023-04-18 22:47:01 +0200

    test numero uno

--- huncutindex.php
+++ huncutindex.php
@@ -5,8 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blender</title>
+    <link href="huncutstyle.css" rel="text/css" type="stylesheet">
 </head>
 <body>
-    
+    <header class="HEAD">
+        head
+    </header>
+
+    <main class="MAIN">
+        blend
+    </main>
+
+    <footer class="FOOT">
+        feet
+    </footer>
 </body>
 </html>
