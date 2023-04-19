7b13a56d2d1f7914d0ad01eb9e7d5a1f88330abf
OGeryan <88555297+OGeryan@users.noreply.github.com>
2023-04-19 08:33:57 +0200

    numero dos

--- huncutindex.php
+++ huncutindex.php
@@ -9,7 +9,12 @@
 </head>
 <body>
     <header class="HEAD STTEXT">
-        head
+        <h1>Blender</h1>
+		<div>
+		<a class="HL-INLINE">Alapok</a>
+		<a class="HL-INLINE">Tutoriál</a>
+		<a class="HL-INLINE">Szimulációk</a>
+		</div>
     </header>
 
     <div class="PANEL-LEFT STTEXT">
@@ -20,11 +25,11 @@
     </div>
 
     <main class="MAIN STTEXT">
-        blend
+        A Blender egy ingyenes és nyílt forráskódú 3D grafikai szoftver, amely lehetővé teszi a felhasználók számára a modellkészítést, az animáció készítést, a textúrázást, a szimulációt és a vizualizációt. A programot széles körben használják a játékfejlesztés, az animációs filmek és az építészet területén. A Blender rendelkezik számos szerszámmal és funkcióval, amelyeket a felhasználók testre szabhatnak és bővíthetnek. Az utóbbi években a Blender jelentősen fejlődött, és az egyik legnépszerűbb 3D grafikai szoftver lett.
     </main>
 
-    <footer class="FOOT STTEXT">
-        feet
+    <footer class="FOOT TTEXT">
+        Blender is a free and open-source 3D graphics software that allows users to create models, animations, textures, simulations, and visualizations. The program is widely used in game development, animation films, and architecture. Blender has a wide range of tools and features that users can customize and expand. In recent years, Blender has made significant progress and has become one of the most popular 3D graphics software.
     </footer>
 </body>
 </html>
